d9da4609dd56394f2968538ad4383e403e49d450
Alexey Onufriev <aleksonufriev@gmail.com>
2020-03-30 00:09:19 +0300

    it adds build

---
+++ build/precache-manifest.3ffcff9273587ffb0c826ee08117a30d.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7bf732c2a785bc770893fbc80c593c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4500586f60cb10dde28",
+    "url": "/static/css/2.fbb034f6.chunk.css"
+  },
+  {
+    "revision": "c930540fa36562df77ba",
+    "url": "/static/css/main.50cfc2f9.chunk.css"
+  },
+  {
+    "revision": "a4500586f60cb10dde28",
+    "url": "/static/js/2.6fb8a3ad.chunk.js"
+  },
+  {
+    "revision": "0bc5220d92bfe34c3dec2f124a7bb286",
+    "url": "/static/js/2.6fb8a3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c930540fa36562df77ba",
+    "url": "/static/js/main.64addffc.chunk.js"
+  },
+  {
+    "revision": "a20fef3f67a5db9b0809",
+    "url": "/static/js/runtime-main.d2e657fd.js"
+  },
+  {
+    "revision": "96677e466cd5a8278d8195c668838fe1",
+    "url": "/static/media/bg.96677e46.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0cc0cdae1de59fc036ae105c0438ac56",
+    "url": "/static/media/les.0cc0cdae.jpg"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "fb069aec13cb81708948500e43cd4626",
+    "url": "/static/media/login.fb069aec.svg"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/static/media/primeicons.e5e0e944.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const axios = require('axios')
 const express = require('express');
-const Controller = require('./controller')
+const bodyParser = require('body-parser')
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello')
+const cheerio = require('cheerio')
+
+const URL_PARSE = 'https://www.science-community.org/ru/conferences'
+const PORT = process.env.PORT || 3001
+
+app.use(bodyParser.json())
+
+app.use(express.static('build'))
+
+app.post('/api/get-cards', async (req, res) => {
+  const { data } = await axios.get(URL_PARSE + encodeURI(req.body.data.link))
+
+  const $ = cheerio.load(data)
+
+  const childrenDiv = $('.panel-col-first.panel-panel > div > div > div.pane-content > div > div.view-content').children().length
+
+  const cards = []
+  for (let i = 1; i <= childrenDiv; i++) {
+    cards.push({
+      title: $(`.panel-col-first.panel-panel > div > div > div > div > div.view-content > div:nth-child(${i}) > div.views-field.views-field-title > span > a`).text(),
+      startDate: $(`.panel-col-first.panel-panel > div > div > div > div > div.view-content > div:nth-child(${i}) > div.views-field.views-field-field-conference-theses-deadlin > div > span`).text(),
+      endDate: $(`.panel-col-first.panel-panel > div > div > div > div > div.view-content > div:nth-child(${i}) > div.views-field.views-field-field-conf-start > div > span`).text(),
+      sity: $(`.panel-col-first.panel-panel > div > div > div > div > div.view-content > div:nth-child(${i}) > div.views-field.views-field-field-city > div`).text()
+    })
+  }
+
+  res.json({
+    cards
+  })
 })
 
-app.get('/api/get-cards', Controller.getCards)
-
-app.listen(3001, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
